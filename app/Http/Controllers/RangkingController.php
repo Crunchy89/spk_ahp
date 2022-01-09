cb4d54e876124640ed9d75e2f421744efde416ad
crunchy89 <rocker.hunt@gmail.com>
2022-01-09 15:06:04 +0800

    perangkingan selesai

--- app/Http/Controllers/RangkingController.php
+++ app/Http/Controllers/RangkingController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Helper\PerhitunganAlternatif;
+use App\Models\Alternatif;
+use App\Models\Kriteria;
 use Illuminate\Http\Request;
 
 class RangkingController extends Controller
@@ -10,8 +13,72 @@ class RangkingController extends Controller
     public function index(Request $request)
     {
         $data = [
-            'title' => 'perangkingan'
+            'title' => 'perangkingan',
+            'alternatif' => Alternatif::all(),
+            'kriteria' => Kriteria::all(),
+            'data' => $this->concat(),
+            'nilai' => $this->nilai(),
+            'rangking' => $this->rangking(),
+            'sortRangking' => $this->sortRangking()
         ];
         return view('admin.rangking.index', $data);
+    }
+    private function data()
+    {
+        $ahp = Kriteria::all();
+        $perhitungan = new PerhitunganAlternatif();
+        $data = [];
+        foreach ($ahp as $k) {
+            array_push($data, $perhitungan->prioritas($k->id));
+        }
+        return $data;
+    }
+    private function concat()
+    {
+        $data = $this->data();
+        $result = [];
+        for ($i = 0; $i < count($data[0]); $i++) {
+            $list = [];
+            for ($j = 0; $j < count($data); $j++) {
+                $list[] = $data[$j][$i];
+            }
+            $result[] = $list;
+        }
+        return $result;
+    }
+    private function nilai()
+    {
+        $data = $this->concat();
+        $bobot = Kriteria::all()->pluck('bobot');
+        $result = [];
+        foreach ($data as $d) {
+            $tempt = [];
+            foreach ($d as $j => $nilai) {
+                array_push($tempt, round($nilai * $bobot[$j], 4));
+            }
+            $result[] = round(array_sum($tempt), 4);
+        }
+        return $result;
+    }
+    private function rangking()
+    {
+        $alternatif = Alternatif::all();
+        $nilai = $this->nilai();
+        $data = [];
+        foreach ($nilai as $i => $val) {
+            $data[] = $val;
+        }
+        return $data;
+    }
+    private function sortRangking()
+    {
+        $alternatif = Alternatif::all();
+        $nilai = $this->nilai();
+        $data = [];
+        foreach ($nilai as $i => $val) {
+            $data[$alternatif[$i]->alternatif] = $val;
+        }
+        arsort($data);
+        return $data;
     }
 }
--- resources/views/admin/rangking/index.blade.php
+++ resources/views/admin/rangking/index.blade.php
@@ -22,7 +22,70 @@
         <div class="card">
             <div class="card-body">
                 <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Alternatif</th>
+                                @foreach ($kriteria as $k)
+                                    <th>{{ $k->kriteria }}</th>
+                                @endforeach
+                                <th>Nilai</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <h3>Hasil perhitungan</h3>
+                            <hr>
+                            <tr>
+                                <td>Bobot P</td>
+                                @foreach ($kriteria as $k)
+                                    <td>{{ $k->bobot }}</td>
+                                @endforeach
+                                <td></td>
+                            </tr>
+                            @foreach ($data as $i=>$d)
+                            <tr>
+                                <td>{{ $alternatif[$i]->alternatif }}</td>
+                                @foreach ($d as $nilai)
+                                    <td>{{ $nilai }}</td>
+                                @endforeach
+                                <td>{{ $rangking[$i] }}</td>
+                            </tr>
+                            @endforeach
+                            <tr>
+                                <td colspan="{{ count($kriteria)+1}}"></td>
+                                <td>{{ round(array_sum($rangking),4) }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-body">
+                <h3>Rangking</h3>
+                <hr>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Alternatif</th>
+                            <th>Nilai</th>
+                            <th>Rangking</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                            $i=1;
+                        @endphp
+                        @foreach ($sortRangking as $key => $value)
+                        <tr>
+                            <td>{{ $key }}</td>
+                            <td>{{ $value }}</td>
+                            <td>{{ $i++ }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
 
